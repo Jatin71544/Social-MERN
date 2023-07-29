783d6510b9aa6f54e4e5929a98ca3c9dd81c000b
Jatin71544 <jatin71544@gmail.com>
2023-07-29 06:58:29 +0530

    Social-MERN first commit

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Chat.css"
+
+function Chat() {
+  return (
+    <div className="chat">
+      <h3>Hey, this is chat</h3>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Sidebar.css"
+//import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>I'm a sidebar</h2>
+      <div className="sidebar_header">
+        <div className="sidebar_headerRight">
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
